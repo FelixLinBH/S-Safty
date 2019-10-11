019a8347a39fb2cbbc1ca9d94f151df1b5a437a9
Felix.lin <fly_81211@hotmail.com>
2019-10-11 23:06:21 +0800

    Added properties and mutating function for Data structure.

--- Classes/CommonData.swift
+++ Classes/CommonData.swift
@@ -49,4 +49,81 @@ public class SyncData {
     @inlinable public init(){
         self.data = Data()
     }
+    
+
 }
+
+
+
+//MARK: Properties
+public extension SyncData {
+    var count: Int {
+        var result = 0
+        queue.sync {
+            result = self.data.count
+        }
+        return result
+    }
+    var isEmpty: Bool {
+        var result = true
+        queue.sync {
+            result = self.data.isEmpty
+        }
+        return result
+    }
+}
+
+//MARK: Mutating
+public extension SyncData {
+    func append(_ bytes: UnsafePointer<UInt8>, count: Int){
+        queue.async(flags: .barrier) {
+            self.data.append(bytes, count: count)
+        }
+    }
+    func append(_ other: Data){
+        queue.async(flags: .barrier) {
+            self.data.append(other)
+        }
+    }
+    func append<SourceType>(_ buffer: UnsafeBufferPointer<SourceType>){
+        queue.async(flags: .barrier) {
+            self.data.append(buffer)
+        }
+    }
+    func append(contentsOf bytes: [UInt8]){
+        queue.async(flags: .barrier) {
+            self.data.append(contentsOf: bytes)
+        }
+    }
+    func append<S>(contentsOf elements: S) where S : Sequence, S.Element == Data.Element{
+        queue.async(flags: .barrier) {
+            self.data.append(contentsOf: elements)
+        }
+    }
+    func resetBytes(in range: Range<Data.Index>){
+        queue.async(flags: .barrier) {
+            self.data.resetBytes(in: range)
+        }
+    }
+    func replaceSubrange(_ subrange: Range<Data.Index>, with data: Data){
+        queue.async(flags: .barrier) {
+            self.data.replaceSubrange(subrange, with: data)
+        }
+    }
+    func replaceSubrange<SourceType>(_ subrange: Range<Data.Index>, with buffer: UnsafeBufferPointer<SourceType>){
+        queue.async(flags: .barrier) {
+            self.data.replaceSubrange(subrange, with: buffer)
+        }
+    }
+    func replaceSubrange<ByteCollection>(_ subrange: Range<Data.Index>, with newElements: ByteCollection) where ByteCollection : Collection, ByteCollection.Element == Data.Iterator.Element{
+        queue.async(flags: .barrier) {
+            self.data.replaceSubrange(subrange, with: newElements)
+        }
+    }
+    func replaceSubrange(_ subrange: Range<Data.Index>, with bytes: UnsafeRawPointer, count cnt: Int){
+        queue.async(flags: .barrier) {
+            self.data.replaceSubrange(subrange, with: bytes, count: cnt)
+        }
+    }
+}
+
